567c69335b68387568aa822230ddf7a4ddbb0f9e
cfrancobedoya <cfrancobedoya@gmail.com>
2017-02-11 09:39:50 -0500

    OPERACIONES ARITMÉTICAS
    Importante el uso de .class

---
+++ operacionesAritmeticas.rb
@@ -0,0 +1,17 @@
+#Operaciones aritméticas
+
+puts 92 * 12  #multiplicación
+puts 1+2 #suma
+puts 3-1 #resta
+puts 8/2 #division
+
+puts 9.pred #imprime el número anterior
+puts 9.next #imprime el siguiente número
+
+puts 8.zero? #Pregunta al 8 si es cero y retorna false
+
+puts 8.odd? #Pregunta si es un número impar, retonar false
+
+puts 8.even? #Pregunta si es un número par, retorna true
+
+puts 8.class #Pregunta que tipo de clase pertenece, devuelve Fixnum (Integer nuevo ruby)
